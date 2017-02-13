0e4a994487df4cbaf59863e4160add1ad9527182
Patrick Conde <patrick.conde.costas@gmail.com>
2017-02-13 12:30:40 +0100

    Params are important

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,35 @@
+class CoachingController < ApplicationController
+  def answer
+    my_message = params["query"]
+    @final_answer = coach_answer_enhanced(my_message)
+  end
+
+  def ask
+  end
+
+  def home
+
+  end
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!"
+      return ""
+    elsif your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    if your_message == "I AM GOING TO WORK RIGHT NOW!"
+      return ""
+    elsif your_message == your_message.upcase
+      return "I can feel your motivation! " + coach_answer(your_message)
+    else
+      return coach_answer(your_message)
+    end
+  end
+
+
+end
